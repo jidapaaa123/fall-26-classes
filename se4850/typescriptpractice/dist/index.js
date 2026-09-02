e6f576741f22efd967741fff5d2a6c30d61115b5
unknown <jidapa.angsutti@ENGINEERING.SNOW>
2026-09-02 15:21:01 -0600

    frontend class

--- se4850/typescriptpractice/dist/index.js
+++ se4850/typescriptpractice/dist/index.js
@@ -1,5 +1,27 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Hello, TypeScript!");
-let n = 10;
+// arrays
+function add(...numbers) {
+    return numbers.reduce((acc, curr) => acc + curr, 0);
+}
+let scores = [85, 42, 111];
+let animals = ["cat", "dog", "spidermonkey"];
+let scores3 = [1, 2, 3];
+let active = [true, true, false];
+scores.push(100);
+// console.log(add(...scores));
+console.log("UPDATED");
+for (let i = 0; i < animals.length; i++) {
+    console.log(animals[i]);
+}
+// map -> changing everything
+let doubled = [];
+for (let number of scores) {
+    doubled.push(number * 2);
+}
+// OR
+let mappedDouble = scores.map((num) => num * 2);
+let mappedAnnounce = animals.map((str) => str + " is a great animal");
+console.log(mappedDouble);
+console.log(mappedAnnounce);
 //# sourceMappingURL=index.js.map
--- se4850/typescriptpractice/src/index.ts
+++ se4850/typescriptpractice/src/index.ts
@@ -1,3 +1,29 @@
-console.log("Hello, TypeScript!");
+// arrays
+function add(...numbers: number[]): number {
+  return numbers.reduce((acc, curr) => acc + curr, 0);
+}
 
-let n:number = 10;
+let scores: number[] = [85,42,111];
+let animals: string[] = ["cat", "dog", "spidermonkey"];
+let scores3: Array<number> = [1,2,3];
+let active: boolean[] = [true, true, false];
+
+scores.push(100);
+// console.log(add(...scores));
+console.log("UPDATED");
+
+for (let i = 0; i < animals.length; i++) {
+  console.log(animals[i]);
+}
+
+// map -> changing everything
+let doubled:number[] = [];
+for (let number of scores) {
+    doubled.push(number * 2);
+}
+// OR
+let mappedDouble:number[] = scores.map((num) => num * 2);
+let mappedAnnounce:string[] = animals.map((str) => str + " is a great animal");
+
+console.log(mappedDouble);
+console.log(mappedAnnounce);

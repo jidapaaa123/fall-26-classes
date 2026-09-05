dee0f18139d56ab488ac47ba4ce7c0aa6576366a
jidapa.angsutti <jidapa.angsutti@students.snow.edu>
2026-09-05 01:38:58 -0600

    remove print debugs

--- se4230/project-1/sorting.py
+++ se4230/project-1/sorting.py
@@ -63,12 +63,8 @@ def insertion_sort(arr):
     for i in range(1, len(arr)):
         curr = arr[i]
         prev = arr[i-1]
-        print(f"Curr: {curr}, i: {i}")
-        print(arr)
         if curr < prev:
-            print(f"It need swap")
             root_insertion_sort(arr, i)
-            print(arr)
     return arr
 
 
